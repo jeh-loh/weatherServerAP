ec24a0dddfdfbb05034d919bdc7b7e313ce301d7
jeh-loh <angelochristian.panopio.568@my.csun.edu>
2024-03-15 20:46:04 -0700

    Added controllers

---
+++ weatherServerAP/DTO/CountryPopulation.cs
@@ -0,0 +1,9 @@
+﻿namespace weatherServerAP.DTO
+{
+    public class CountryPopulation
+    {
+        public required string Name { get; set;}
+        public int CountryId { get; set; }
+        public int Population { get; set; }
+    }
+}
